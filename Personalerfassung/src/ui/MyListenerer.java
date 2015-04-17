e33b150e7192a73cfbc751745a10e08a337c4083
Schukardt <Schukardt@MPR31229.ab.ba.ba-ravensburg.de>
2015-04-17 12:42:39 +0200

    work

--- Personalerfassung/src/ui/MyListenerer.java
+++ Personalerfassung/src/ui/MyListenerer.java
@@ -13,11 +13,14 @@ public class MyListenerer implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println(e.getActionCommand());
+		if (e.getActionCommand().equals("OK!")){
 		Person pp = new Person();
 		pp.setNachname(fpf2.getNachnamefield().getText()) ;
 		pp.setVorname(fpf2.getVornamefield().getText()) ;
-		
+		System.out.print(pp.getVorname());
+		System.out.print(" ");
+		System.out.println(pp.getNachname());
+		}
 		
 		
 		
