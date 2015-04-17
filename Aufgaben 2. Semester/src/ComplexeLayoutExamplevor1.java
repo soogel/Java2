2ebafd60983781bd19ea3e3e44626c19b723757a
Schukardt <Schukardt@MPR31229.ab.ba.ba-ravensburg.de>
2015-04-17 10:27:42 +0200

    x

--- Aufgaben 2. Semester/src/ComplexeLayoutExamplevor1.java
+++ Aufgaben 2. Semester/src/ComplexeLayoutExamplevor1.java
@@ -1,12 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
@@ -26,6 +30,7 @@ public class ComplexeLayoutExamplevor1 extends JFrame{
 		p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
 
 		JLabel summaryText = new JLabel("Summary");
+		summaryText.setAlignmentX(LEFT_ALIGNMENT);
 		p1.add(summaryText);
 		
 		
@@ -40,7 +45,29 @@ public class ComplexeLayoutExamplevor1 extends JFrame{
 		p1.add(Box.createVerticalStrut(25));
 		
 		JLabel descriptionText = new JLabel("Text");
+		descriptionText.setAlignmentX(LEFT_ALIGNMENT);
 		p1.add(descriptionText);
+		
+		JPanel p3 = new JPanel();
+		JScrollPane jsp = new JScrollPane();
+		p3.setLayout(new BorderLayout());
+		JTextArea textArea = new JTextArea();
+		//p3.add(textArea);
+		jsp.setViewportView(textArea);
+		p3.add(jsp);
+		p3.setAlignmentX(LEFT_ALIGNMENT);
+		p1.add(p3);
+		
+		JPanel p4 = new JPanel();
+		p4.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		JButton send = new JButton("SEND");
+		JButton cancel = new JButton("Cancel");
+		
+		p4.add(send);
+		p4.add(cancel);
+		p4.setAlignmentX(LEFT_ALIGNMENT);
+		
+		p1.add(p4);
 		
 		
 	}
